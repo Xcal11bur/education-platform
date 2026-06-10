b1bf0474ff0ac08d201bb587984743f4162476b0
Xal1bur <977899943@qq.com>
2026-06-10 18:17:00 +0800

    chore: disable swagger by default

--- backend/src/main/java/com/education/platform/config/OpenApiConfig.java
+++ backend/src/main/java/com/education/platform/config/OpenApiConfig.java
@@ -5,10 +5,12 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnProperty(prefix = "springdoc.api-docs", name = "enabled", havingValue = "true")
 public class OpenApiConfig {
 
     @Bean
